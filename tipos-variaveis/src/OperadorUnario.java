470caddb4ce7dfa1d2c41d865a1a4d5500108a20
BergSabbath <Bergit7@gmail.com>
2024-05-25 08:36:56 -0300

    apenas treinando

---
+++ tipos-variaveis/src/OperadorUnario.java
@@ -0,0 +1,17 @@
+public class OperadorUnario {
+
+   public static void main(String[] args) {
+
+    // numero ++, numero --
+    
+   
+    int a, b;
+
+    a = 5;
+    b = 6;
+
+    String resultado = a==b ? "verdadeiro" : "falso";
+    
+    System.out.println(resultado);
+   }
+}
