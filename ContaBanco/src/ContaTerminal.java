af38f1dcb4fbc022e9c98d2bc5b494790df21693
BergSabbath <Bergit7@gmail.com>
2024-05-27 22:19:17 -0300

    Finalizando o pequeno desafio

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -2,19 +2,32 @@ import java.util.Scanner;
 
 public class ContaTerminal {
 
-      public static void main(String[] args) {
+    public static void main(String[] args) {
 
         int numero;
-        String agencia, NomeCliente;
+        String agencia, nomeCliente;
         double saldo;
 
         Scanner teclado = new Scanner(System.in);
-        
+
         System.out.println("Digite o numero da agencia: ");
-            agencia = teclado.nextLine();
-    
+        agencia = teclado.nextLine();
 
-    } 
+        System.out.println("Digite o numero de sua conta:");
+        numero = teclado.nextInt();
 
-    
+        System.out.println("Digite Seu Nome:");
+        nomeCliente = teclado.next();
+
+        System.out.println("Digite seu saldo:");
+        saldo = teclado.nextDouble();
+
+
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agencia é \n" 
+        + agencia +" conta "+ numero + " e o seu saldo " + saldo + " já está disponivel para saque."
+        );
+
+
+    }
+
 }
