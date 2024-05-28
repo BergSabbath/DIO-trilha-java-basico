13b2aebfb26b020b9201b1058abb884ad4580d1e
BergSabbath <Bergit7@gmail.com>
2024-05-27 21:36:54 -0300

    Criando o banco, desafio do projeto

---
+++ ContaBanco/src/ContaTerminal.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class ContaTerminal {
+
+      public static void main(String[] args) {
+
+        int numero;
+        String agencia, NomeCliente;
+        double saldo;
+
+        Scanner teclado = new Scanner(System.in);
+        
+        System.out.println("Digite o numero da agencia: ");
+            agencia = teclado.nextLine();
+    
+
+    } 
+
+    
+}
